0f0ee047a2aa514fd81dfa71aa6859c4d4eac7a0
Vicus Combrinck <combrinck.v@belgiumcampus.ac.za>
2018-02-21 11:21:53 +0200

    NoSQL is the best kind of SQL, Added initial controller functionality and model. DbContext also added.

---
+++ BusinessLogic/Controllers/DocumentController.cs
@@ -0,0 +1,23 @@
+﻿using MongoAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Controllers
+{
+    public class DocumentController 
+    {
+        //public void async POST(Document Document)
+        //{
+        //     await 
+        //}
+
+        public void POST()
+        {
+            var document = new Document();
+
+        }
+    }
+}
---
+++ MongoAccess/DbContext/DataBaseContext.cs
@@ -0,0 +1,47 @@
+﻿using MongoAccess.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoAccess.DbContext
+{
+    public class DataBaseContext
+    {
+        MongoClient client = new MongoClient("Projects.belgiumcampus:27017");
+
+        public void Insert()
+        {
+            var database = client.GetDatabase("PotatoFactoryCRF");
+            database.CreateCollection("");
+        }
+
+
+
+        public async Task<List<BsonDocument>> GetBsonDocuments(string id)
+        {
+            List<BsonDocument> Documents = new List<BsonDocument>();
+            var database = client.GetDatabase("PotatoFactoryCRF");
+            var collection = database.GetCollection<BsonDocument>(id);
+            await collection.Find(new BsonDocument()).ForEachAsync(d => Documents.Add(d));
+            return Documents;
+        }
+
+        public async Task<List<BsonDocument>> GetDocumentswithFilter(int value)
+        {
+            List<BsonDocument> Documents = new List<BsonDocument>();
+            var filter = Builders<BsonDocument>.Filter.Eq("", value);
+            var database = client.GetDatabase("PotatoFactoryCRF");
+            var collection = database.GetCollection<BsonDocument>(value.ToString());
+            var cursor = collection.Find(filter).ToCursor();
+            foreach (var document in cursor.ToEnumerable())
+            {
+                 Documents.Add(document);
+            }
+            return Documents;
+        }
+    }
+}
